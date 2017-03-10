0e38ddfa20a161d1aa546aa3f82b7d0e845813ad
Alex Dewar <a.dewar@sussex.ac.uk>
2017-03-10 18:11:37 +0000

    start work on discussion

---
+++ discussion.mm
@@ -0,0 +1,145 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="discussion" FOLDED="false" ID="ID_367236182" CREATED="1489148420809" MODIFIED="1489148427292" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.909">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="old" POSITION="right" ID="ID_574978180" CREATED="1489148469669" MODIFIED="1489148471213">
+<edge COLOR="#0000ff"/>
+<node TEXT="1. general intro" ID="ID_1545146606" CREATED="1489148471215" MODIFIED="1489148477934">
+<node TEXT="computational approaches enable relating neural circuitry to behaviour" ID="ID_1342520803" CREATED="1489148477936" MODIFIED="1489148513847"/>
+<node TEXT="seelig and jayaraman" ID="ID_1036942041" CREATED="1489148517814" MODIFIED="1489148532745"/>
+<node TEXT="outputs of RFs can support bar homing and limited pattern recognition" ID="ID_1856694387" CREATED="1489148533118" MODIFIED="1489148575040"/>
+</node>
+<node TEXT="2. pattern recognition" ID="ID_1542684768" CREATED="1489148578591" MODIFIED="1489148589176">
+<node TEXT="we know flies can discriminate patterns differing along certain params" ID="ID_542254075" CREATED="1489148589178" MODIFIED="1489148628714"/>
+<node TEXT="but we also know that the encoding is independent of any single param!" ID="ID_267824715" CREATED="1489148628880" MODIFIED="1489148652289"/>
+<node TEXT="R2s don&apos;t seem to be pattern recognition cells" ID="ID_468983765" CREATED="1489148652792" MODIFIED="1489148680009"/>
+</node>
+<node TEXT="3. bars" ID="ID_289963735" CREATED="1489148754498" MODIFIED="1489148768178">
+<node TEXT="cells are tuned to the vertical" ID="ID_481028894" CREATED="1489148768180" MODIFIED="1489148796060"/>
+<node TEXT="describe buridan&apos;s paradigm" ID="ID_1483621691" CREATED="1489148796659" MODIFIED="1489148803668"/>
+<node TEXT="neuser paper" ID="ID_499260595" CREATED="1489148809827" MODIFIED="1489148816804"/>
+<node TEXT="head to edges of large bars, centres of smaller ones" ID="ID_861558808" CREATED="1489148817059" MODIFIED="1489148843109"/>
+<node TEXT="we show could be used for a task" ID="ID_490564374" CREATED="1489148861476" MODIFIED="1489148870868"/>
+<node TEXT="mention azimuth parameter of ANNs" ID="ID_55730896" CREATED="1489148871556" MODIFIED="1489148941094"/>
+<node TEXT="detailed description of neuroscience" ID="ID_1826813560" CREATED="1489148959533" MODIFIED="1489148978478">
+<node TEXT="R4s could be for path integration or like HD cells" ID="ID_663182320" CREATED="1489148978479" MODIFIED="1489148992439"/>
+</node>
+</node>
+<node TEXT="ANNs not really discussed" ID="ID_1516881956" CREATED="1489149024214" MODIFIED="1489149031064"/>
+</node>
+<node TEXT="new" POSITION="right" ID="ID_24874181" CREATED="1489149033526" MODIFIED="1489149035415">
+<edge COLOR="#00ff00"/>
+<node TEXT="1. general intro as before" ID="ID_1233216634" CREATED="1489149402701" MODIFIED="1489149417446"/>
+<node TEXT="combine pattern and bar sections?" ID="ID_971853792" CREATED="1489149417741" MODIFIED="1489149426246"/>
+<node TEXT="2. pattern recognition" ID="ID_883245789" CREATED="1489149426493" MODIFIED="1489149444494">
+<node TEXT="pattern recognition doesn&apos;t seem to be important for flies (cf. bees), though we know they have SOME ability" ID="ID_1567004454" CREATED="1489149444495" MODIFIED="1489149534618"/>
+<node TEXT="a) might be by-product of other behaviours" ID="ID_564557624" CREATED="1489149535375" MODIFIED="1489149552904"/>
+<node TEXT="b) danger of interpreting ability to discriminate as a &quot;discrimination ability&quot;" ID="ID_1847418720" CREATED="1489149553343" MODIFIED="1489149587369"/>
+<node TEXT="cite biosystems - you can use the info in biologically relevant ways" ID="ID_1510954182" CREATED="1489149587888" MODIFIED="1489149608913"/>
+<node TEXT="arguments from..." ID="ID_1571807817" CREATED="1489149946974" MODIFIED="1489150104850">
+<node TEXT="ecology" ID="ID_871603234" CREATED="1489149954424" MODIFIED="1489149972903">
+<node TEXT="bees need to be able to discriminate complex visual patterns (flies), fruitflies not so much" ID="ID_1669779034" CREATED="1489149972904" MODIFIED="1489150000073"/>
+<node TEXT="D. have smaller eyes, brains" ID="ID_789065361" CREATED="1489150137002" MODIFIED="1489150149803"/>
+<node TEXT="find: ev&apos;ce about processing of vis stim" ID="ID_1752656076" CREATED="1489150150642" MODIFIED="1489150162627"/>
+</node>
+<node TEXT="physiology" ID="ID_1640684926" CREATED="1489150067089" MODIFIED="1489150069682">
+<node TEXT="accordingly they have different physiology, reflecting different selection pressures" ID="ID_578644531" CREATED="1489150000527" MODIFIED="1489150039385"/>
+</node>
+<node TEXT="behavioural evidence" ID="ID_79046227" CREATED="1489150079113" MODIFIED="1489150133578">
+<node TEXT="find: ev&apos;ce fm literature" ID="ID_144857109" CREATED="1489150166658" MODIFIED="1489150193115"/>
+</node>
+<node TEXT="just because an animal can do X doesn&apos;t mean it has an &quot;X ability&quot;" ID="ID_1494069370" CREATED="1489150206963" MODIFIED="1489150236804">
+<node TEXT="at least not an innate one, arrived at by selection pressure for this ability" ID="ID_1646701171" CREATED="1489150277252" MODIFIED="1489150302573"/>
+</node>
+</node>
+</node>
+<node TEXT="3. bars" ID="ID_1639764219" CREATED="1489149612232" MODIFIED="1489149615273">
+<node TEXT="condense detail of R4s" ID="ID_526056175" CREATED="1489149615274" MODIFIED="1489149651514"/>
+</node>
+<node TEXT="we need an outro" ID="ID_1246826545" CREATED="1489149659705" MODIFIED="1489149669770">
+<node TEXT="modelling + new biological data is way forward" ID="ID_1936513846" CREATED="1489149669772" MODIFIED="1489149716507"/>
+<node TEXT="yada yada" ID="ID_112677510" CREATED="1489149717194" MODIFIED="1489149719875"/>
+</node>
+</node>
+</node>
+</map>
