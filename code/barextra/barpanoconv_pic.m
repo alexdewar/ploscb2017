4b518179fc438d9d2595c64e10dccad5fcf147ff
Alex Dewar <a.dewar@sussex.ac.uk>
2017-07-31 11:40:23 +0100

    plots of indiv values

--- code/barextra/barpanoconv_pic.m
+++ code/barextra/barpanoconv_pic.m
@@ -61,16 +61,34 @@ for i = 1:length(imfns)
     vals_r4_r(:,end+1) = vals_r4_r(:,1);
 
     figure(i);clf
-    hold on
-    plot(ths,mean(vals_r2_l),ths,mean(vals_r2_r),'b--')
-    plot(ths,mean(vals_r4_l),'g',ths,mean(vals_r4_r),'g--')
-
-    xlim([ths(1) ths(end)])
-
-    set(gca,'FontSize',8,'XTick',-180:180:180); %,'YTick',-0.5:0.5:0.5);
+    
+    subplot(5,1,1)
+    imagesc(im)
+    axis off
+    
+    subplot(5,1,2)
+    indivplot(ths,vals_r2_l,'R2 L');
+    
+    subplot(5,1,3)
+    indivplot(ths,vals_r2_r,'R2 R');
+    
+    subplot(5,1,4)
+    indivplot(ths,vals_r4_l,'R4 L');
+    
+    subplot(5,1,5)
+    indivplot(ths,vals_r4_r,'R4 R');
 
     if dosave
         savefig(sprintf('vw_barpic_%s',imfn(1:end-4)),[20 6]);
     end
 
 end
+
+function indivplot(ths,vals,label)
+hold on
+plot(ths,vals)
+plot(ths,mean(vals),'k--','LineWidth',3);
+xlim([ths(1) ths(end)])
+ylim([-0.5 0.5])
+set(gca,'FontSize',8,'XTick',-180:45:180);
+title(label)
