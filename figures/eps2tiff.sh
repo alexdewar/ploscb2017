e7cb1244a3ac1d2620b29a110e9b67f99023f91e
Alex Dewar <a.dewar@sussex.ac.uk>
2017-10-13 10:56:55 +0100

    delete eps files and script

--- figures/eps2tiff.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# converts the EPS files to TIFF files formatted in the way PLOS want
-mogrify -font Arial -density 600 -format tiff -colorspace rgb -compress lzw -depth 8 -alpha off *.eps
